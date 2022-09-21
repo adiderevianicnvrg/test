83b4dc535ae40460e02115d5cd98fe68fc192386
adiderevianicnvrg <94552665+adiderevianicnvrg@users.noreply.github.com>
2022-09-20 22:46:44 -0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,32 +1,6 @@
-from cnvrg import Endpoint
-# from imageio import imread
-import imageio
-from skimage.transform import resize
-# from scipy.misc.pilutil import imread, imresize
-import numpy as np
-import tensorflow as tf
-import keras
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-#perform the prediction
-from keras.models import load_model
-#include custom charts in logging
-e = Endpoint()
-model = load_model('output/mnist_model.h5')
-
-# load an image and predict the class
-def predict(file_path):
-    x = imageio.imread(file_path, mode='L')
-    #compute a bit-wise inversion so black becomes white and vice versa
-    x = np.invert(x)
-    #make it the right size
-    x = resize(x,(28,28))
-    #convert to a 4D tensor to feed into our model
-    x = x.reshape(1,28,28,1)
-    x = x.astype('float32')
-    x /= 255
-    # predict the class
-    out = model.predict(x)
-    #log the predicted digit
-    e.log_metric("digit", np.argmax(out))
-    return str(np.argmax(out))
+def predict(sentence):
+    encoded = encode_sentence(sentence)
+    pred = np.array([encoded])
+    pred = vectorize_sequences(pred)
+    a = model.predict(pred)
+    return str(a[0][0])
